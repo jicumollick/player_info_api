dcb6d86c216925321709d4862e18c072161773a0
jicu mollick <jikumollick82@gmail.com>
2022-02-28 17:24:11 +0600

    all error handling done

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-
-
 const allPlayer = () => {
+
+    document.getElementById('player-container').innerHTML = '';
     const searchValue = document.getElementById('search-box').value;
+    document.getElementById('search-box').value = '';
     
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`; 
 
+ 
+
     fetch(url)
     .then(res => res.json())
-    .then(data => showPlayerDetails(data.player));
+    .then(data => {
+        console.log(data);
+        if(data.player === null){
+    const parent = document.getElementById('player-container');
+            
+           
+            const p = document.createElement('p');
+            p.innerText = "not available";
+            p.classList.add('text-danger');
+            parent.appendChild(p);
+             
+        }else{
+    
+        showPlayerDetails(data.player);
+    }
+});
 };
 
 const showPlayerDetails = (players) => {
@@ -17,51 +35,48 @@ const showPlayerDetails = (players) => {
         const div = document.createElement('div');
     
         div.innerHTML = `
-        <div class="card border p-5">
+        <div class="card border p-5 mt-3">
         <div class="pro-pic">
-            <img class="img-fluid w-50" src="${player.strThumb}" alt="">
+            <img class="img-fluid w-50" src="${player.strThumb}" alt="Image Not Available">
         </div>
         <h2> Name: ${player.strPlayer} </h2>
         <h5> Country: ${player.strNationality} </h5>
         <p></p>
         <div class="all-btn">
-            <button class="btn btn-danger">Delete</button>
+            <button onclick="deleteMe(event)" class="btn btn-danger">Delete</button>
             <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
         </div>
     
     </div>
-        `;
-    
+        `;    
         parent.appendChild(div);
     }
-
 }
-
 const details = (id) => {
 
     const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
     fetch(url)
     .then(res => res.json())
     .then(data => setDetails(data.players[0]));
-
 }
 
 const setDetails = (info) => {
 
+    console.log(info);
     document.getElementById('detail-container').innerHTML = `
     <div class="card border p-5">
     <div class="pro-pic">
-        <img class="img-fluid w-50" src="${info.strThumb}" alt="">
+        <img class="img-fluid w-50" src="${info.strThumb}" alt="image not available">
     </div>
     <h2> Name: ${info.strPlayer} </h2>
     <h5> Country: ${info.strNationality} </h5>
-    <p></p>
-
+    <h5> Gender: ${info.strGender} </h5>
+    <h5> Team: ${info.strTeam} </h5>
+    <p>Description: ${info.strDescriptionEN} </p>
     </div>
-    
     `;
+}
 
-
-
-
+const deleteMe = (event) => {
+    (event.target.parentNode.parentNode).style.display = 'none';
 }
